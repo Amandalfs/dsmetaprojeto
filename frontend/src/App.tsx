014c74e8af937e2b1a4400afb1cb017f5cb0ca19
Amanda Eduarda <caduc57@gmail.com>
2022-11-16 11:47:46 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import Header from "./components/Header"
+import Header from './components/Header/'
 import SalesCard from './components/SalesCard'
 
 
 function App() {
   return (
     <>
-      <Header />
+      <Header/>
       <main>
         <section id="sales">
           <div className="dsmeta-container">
-            <SalesCard />
+            <SalesCard/>
           </div>
         </section>
       </main>
